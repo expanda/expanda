0e7f3a735a1955b8c155a6af9275e0b7772626a0
t04632hn@cs0.bioinfo.i.ttck.keio.ac.jp <t04632hn@cs0.bioinfo.i.ttck.keio.ac.jp>
2008-11-16 20:28:33 +0900

    GML parser fixed.

--- lib/eXpanda/Data/GML.pm
+++ lib/eXpanda/Data/GML.pm
@@ -26,93 +26,92 @@ str is graphics structure.
 =cut
 
 sub Gml2Str{
-  my $gmlfile = shift;
-  my $gstr;
-  my $gra = 0;
-  my $type = 0;
-  my $id = 0;
-  my $source = 0;
-  my $target = 0;
-  my $key = 0;
-  my $val = 0;
+	my $gmlfile = shift;
+	my $gstr;
+	my $gra = 0;
+	my $type = 0;
+	my $id = 0;
+	my $source = 0;
+	my $target = 0;
+	my $key = 0;
+	my $val = 0;
 
-  my $dir = undef;
+	my $dir = undef;
 
-  open(F, "$gmlfile");
-  while (<F>) {
-    chomp;
-    if (/^\s+node/) {
-      $type = 'node';
-      $gra = 0;
-    } elsif (/^\s+edge/) {
-      $type = 'edge';
-      $gra = 0;
-    } elsif (/graphics/) {
-      $gra = 1;
-    } elsif (/^\s+?(label)\s+?(.*)/ && $type eq 'node') {
-      $key = $1;
-      $val = $2;
-      $val =~ s/\"//g;
-      $gstr->{$type}->{$id}->{$key} = $val;
-    } elsif (/^\s+?([^\s+?]*)\s+?(.*)/ && $gra != 1 && $type eq 'node') {
-      $key = $1;
-      $val = $2;
-      $val =~ s/\"//g;
-      print "$1\t$2\n";
-      if ($key eq 'id' || $key eq 'root_index') {
-	$id = $val;
-      }
+	open(F, "$gmlfile");
+	while (<F>) {
+		chomp;
+		if (/^\s+node/) {
+			$type = 'node';
+			$gra = 0;
+		} elsif (/^\s+edge/) {
+			$type = 'edge';
+			$gra = 0;
+		} elsif (/graphics/) {
+			$gra = 1;
+		} elsif (/^\s+?(label)\s+?(.*)/ && $type eq 'node') {
+			$key = $1;
+			$val = $2;
+			$val =~ s/\"//g;
+			$gstr->{$type}->{$id}->{$key} = $val;
+		} elsif (/^\s+?([^\s+?]*?)\s+?(\S*?)$/ && $gra != 1 && $type eq 'node') {
+			$key = $1;
+			$val = $2;
+			$val =~ s/\"//g;
+			if ($key eq 'id' || $key eq 'root_index') {
+				$id = $val;
+			}
 
-      $gstr->{$type}->{$id}->{$key} = $val;
-    } elsif (/^\s+?([^\t]*)\s+?(.*)/ && $gra != 1 && $type eq 'edge') {
-      $key = $1;
-      $val = $2;
-      $val =~ s/\"//g;
-      if ($key eq 'source') {
-	$source = $val;
-      } elsif ($key eq 'target') {
-	$target = $val;
-      } else {
-	$gstr->{$type}->{$source}->{$target}->{$key} = $val;
-	$gstr->{$type}->{$source}->{$target}->{source} = $source;
-	$gstr->{$type}->{$source}->{$target}->{target} = $target;
-      }
-    } elsif (/^s+?([^\t]*)\s+?(.*)/ && $gra == 1 && $type) {
-      $key = $1;
-      $val = $2;
-      $val =~ s/\"//g;
+			$gstr->{$type}->{$id}->{$key} = $val;
+		} elsif (/^\s+?([^\t]*)\s+?(\S*?)$/ && $gra != 1 && $type eq 'edge') {
+			$key = $1;
+			$val = $2;
+			$val =~ s/\"//g;
+			if ($key eq 'source') {
+				$source = $val;
+			} elsif ($key eq 'target') {
+				$target = $val;
+			} else {
+				$gstr->{$type}->{$source}->{$target}->{$key} = $val;
+				$gstr->{$type}->{$source}->{$target}->{source} = $source;
+				$gstr->{$type}->{$source}->{$target}->{target} = $target;
+			}
+		} elsif (/^s+?([^\t]*)\s+?(.*)/ && $gra == 1 && $type) {
+			$key = $1;
+			$val = $2;
+			$val =~ s/\"//g;
 
-      #follows are comversion for svg
-      $key =~ s/outline/stroke/g;
-      $key =~ s/\_/\-/g;
-      $val = 'oval' if($key eq 'type');
+			#follows are comversion for svg
+			$key =~ s/outline/stroke/g;
+			$key =~ s/\_/\-/g;
+			$val = 'oval' if($key eq 'type');
 
-      $gstr->{$type}->{$id}->{graphics}->{$key} = $val if($type eq 'node');
-      if ($type eq 'edge') {
-	if ($key eq 'width') {
-	  $gstr->{$type}->{$source}->{$target}->{graphics}->{'stroke-width'} = $val;
-	} else {
-	  $gstr->{$type}->{$source}->{$target}->{graphics}->{$key} = $val;
+			$gstr->{$type}->{$id}->{graphics}->{$key} = $val if($type eq 'node');
+			if ($type eq 'edge') {
+				if ($key eq 'width') {
+					$gstr->{$type}->{$source}->{$target}->{graphics}->{'stroke-width'} = $val;
+				} else {
+					$gstr->{$type}->{$source}->{$target}->{graphics}->{$key} = $val;
+				}
+			}
+		}
 	}
-      }
-    }
-  }
-  close F;
+	close F;
 
-  delete $gstr->{node}->{''};
-  delete $gstr->{edge}->{''};
-  delete $gstr->{node}->{'0'};
-  delete $gstr->{edge}->{'0'};
+	delete $gstr->{node}->{''};
+	delete $gstr->{edge}->{''};
+	delete $gstr->{node}->{'0'};
+	delete $gstr->{edge}->{'0'};
 
-  print Dumper $gstr;
+	print Dumper $gstr;
 
-  $gstr->{graphed} = 1;
-  my @edgekey = keys %{$gstr->{edge}};
-  croak qq{[fatal] No network imported from "$gmlfile" for gml format. check import file;} if($#edgekey == -1);
+	$gstr->{graphed} = 1;
+	my @edgekey = keys %{$gstr->{edge}};
+	croak qq{[fatal] No network imported from "$gmlfile" for gml format. check import file;} if($#edgekey == -1);
 
-  $gstr->{filetype} = 'gml';
+	$gstr->{filetype} = 'gml';
 
-  return $gstr;
+	return $gstr;
 }
 
 1;
